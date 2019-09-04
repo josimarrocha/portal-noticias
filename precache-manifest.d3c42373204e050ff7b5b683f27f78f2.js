9d1457a95cc15c05ea98c64337321702d669107b
josimarrocha <rocha_josimar@yahoo.com.br>
2019-09-04 23:19:38 +0000

    Updates

---
+++ precache-manifest.d3c42373204e050ff7b5b683f27f78f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aa6402674b8ba68ba86e6ea4061596e",
+    "url": "/portal-noticias/index.html"
+  },
+  {
+    "revision": "c5e92a6e23e111659cf8",
+    "url": "/portal-noticias/static/css/2.09d751c7.chunk.css"
+  },
+  {
+    "revision": "0db616e576f5b5ba4c29",
+    "url": "/portal-noticias/static/css/main.71ea2e94.chunk.css"
+  },
+  {
+    "revision": "c5e92a6e23e111659cf8",
+    "url": "/portal-noticias/static/js/2.7f5596cb.chunk.js"
+  },
+  {
+    "revision": "0db616e576f5b5ba4c29",
+    "url": "/portal-noticias/static/js/main.54ea3855.chunk.js"
+  },
+  {
+    "revision": "af49fa45310a2c5599f0",
+    "url": "/portal-noticias/static/js/runtime~main.6f603e82.js"
+  }
+]);
