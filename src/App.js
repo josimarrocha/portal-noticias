45c1f820c4117588f98509ab11346581c212426e
Josimar <rocha_josimar@yahoo.com.br>
2019-09-04 20:02:16 -0300

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './components/Header'
+import Noticias from './components/Noticias'
+import Formulario from './components/Formulario'
+import './index.css'
+
+class App extends Component {
+  state = {
+    noticias: []
+  }
+
+  componentWillMount() {
+    this.carregaNoticias()
+  }
+
+  carregaNoticias = async (categoria = 'general') => {
+    const url = `https://newsapi.org/v2/top-headlines?country=br&category=${categoria}&apiKey=dc9e6a20167344d2b04dd1aebf8ad614`
+
+    const data = await fetch(url)
+    const response = await data.json()
+
+    this.setState({ noticias: response.articles })
+  }
+
+  render() {
+    const { noticias } = this.state
+    return (
+      <div className="App">
+        <Header />
+        <div className='container white container-noticias'>
+          <Formulario carregaNoticias={this.carregaNoticias} />
+          {!!noticias.length && <Noticias noticias={this.state.noticias} />}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
